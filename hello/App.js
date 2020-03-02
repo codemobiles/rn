e12bcc3af4d55de28d4d6887e6fd49788c906159
codemobiles <chaiyasit.t@gmail.com>
2020-03-02 13:39:13 +0700

    1234

--- hello/App.js
+++ hello/App.js
@@ -2,39 +2,17 @@ import React from 'react';
 import {View, Text, Button, TextInput} from 'react-native';
 
 const App = () => {
-
-
-  
   return (
     <View
       style={{
-        backgroundColor: '#F00',
         flexDirection: 'column',
         flex: 1,
-        justifyContent: 'space-evenly',
+        justifyContent: 'flex-start',
         alignItems: 'stretch',
       }}>
-
-      <TextInput placeholder="Name"/>
-      <Button title="RUN" onPress={()=>alert("Hey")} />
-      <Text
-        style={{flex: 1, color: '#FFF', fontSize: 10, backgroundColor: '#000'}}>
-        CodeMobiles
-      </Text>
-
-      <Text
-        style={{flex: 1, color: '#FFF', fontSize: 30, backgroundColor: '#0ff'}}>
-        CodeMobiles
-      </Text>
-      <Text
-        style={{flex: 1, color: '#FFF', fontSize: 30, backgroundColor: '#000'}}>
-        CodeMobiles
-      </Text>
-
-      <Text
-        style={{flex: 1, color: '#FFF', fontSize: 30, backgroundColor: '#0ff'}}>
-        CodeMobiles
-      </Text>
+      <TextInput placeholder="Name" />
+      <Button title="RUN" />
+      <Text>xxx</Text>
     </View>
   );
 };
