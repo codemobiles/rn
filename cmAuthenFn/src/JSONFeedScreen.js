5a379260b611dcdddc8f1fddc08a258a619e9f81
codemobiles <chaiyasit.t@gmail.com>
2020-03-03 11:59:24 +0700

    1234

--- cmAuthenFn/src/JSONFeedScreen.js
+++ cmAuthenFn/src/JSONFeedScreen.js
@@ -38,4 +38,44 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
+  list_header: {
+    width: '100%',
+    height: 100,
+  },
+  listCard: {
+    overflow: 'hidden',
+    flexDirection: 'column',
+    marginTop: 20,
+    marginLeft: 20,
+    marginRight: 20,
+    backgroundColor: '#FFF',
+    borderRadius: 20,
+    padding: 0,
+  },
+  listCardView: {
+    flexDirection: 'row',
+    marginBottom: 16,
+    height: 45,
+    alignItems: 'center',
+  },
+  listAvatar: {
+    width: 45,
+    height: '100%',
+    marginRight: 16,
+  },
+  listTitleSubtitleContainer: {
+    flexDirection: 'column',
+    marginRight: 16,
+    flex: 1,
+  },
+  listTitle: {
+    fontWeight: '700',
+  },
+  listSubTitle: {
+    fontWeight: '100',
+  },
+  listYoutbeImage: {
+    width: '100%',
+    height: 190,
+  },
 });
