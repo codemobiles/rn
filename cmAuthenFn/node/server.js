5c75ba5b6bfcc7ca4f6e0a3c1e10338faf6bd838
codemobiles <chaiyasit.t@gmail.com>
2020-03-04 11:55:03 +0700

    1234

--- cmAuthenFn/node/server.js
+++ cmAuthenFn/node/server.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const formidable = require('formidable');
 const app = express();
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.json({result: 'I am root folder'});
+});
+
+app.post('/upload', (req, res) => {
+  let form = new formidable.IncomingForm();
+  form.parse(req, (error, fields, files) => {
+    res.json({error, fields, files});
+  });
 });
 
 app.get('/login', (req, res) => {
