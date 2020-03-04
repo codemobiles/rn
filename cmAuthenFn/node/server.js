1d3d1693af2f0fa7af1a00552bf1f14f61ab4ecf
codemobiles <chaiyasit.t@gmail.com>
2020-03-04 11:37:23 +0700

    1234

--- cmAuthenFn/node/server.js
+++ cmAuthenFn/node/server.js
@@ -1,1 +1,14 @@
-console.log('lek');
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.json({result: 'I am root folder'});
+});
+
+app.get('/login', (req, res) => {
+  res.json({result: 'I am login folder'});
+});
+
+app.listen(3000, () => {
+  console.log('Running...');
+});
