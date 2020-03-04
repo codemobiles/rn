976167b9d14c7c16879a6649545c917a25204b6e
codemobiles <chaiyasit.t@gmail.com>
2020-03-04 11:51:34 +0700

    1234

--- cmAuthenFn/backend/readme.txt
+++ cmAuthenFn/backend/readme.txt
@@ -3,3 +3,5 @@ npm init -y
 npm i express body-parser formidable fs-extra
 
 http://localhost:3000/
+
+{"username":"admin", "password": "1234"}
--- cmAuthenFn/node/server.js
+++ cmAuthenFn/node/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.json({result: 'I am root folder'});
@@ -7,6 +9,10 @@ app.get('/', (req, res) => {
 
 app.get('/login', (req, res) => {
   res.json({result: 'login', detail: req.query});
+});
+
+app.post('/register', (req, res) => {
+  res.json({result: 'register', detail: req.body});
 });
 
 app.listen(3000, () => {
