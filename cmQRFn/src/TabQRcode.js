77c50abf2d6fd09a727ca2f100c223e45d814968
codemobiles <chaiyasit.t@gmail.com>
2020-03-04 15:40:44 +0700

    1234

--- cmQRFn/src/TabQRcode.js
+++ cmQRFn/src/TabQRcode.js
@@ -14,10 +14,12 @@ import QRCode from 'react-native-qrcode-svg';
 const PATH_TO_LOGO = './assets/img/cmdev_icon.png';
 
 const TabQRcode = () => {
+  const [qrValue, setQRValue] = React.useState('');
+
   return (
     <View style={{flex: 1, flexDirection: 'column'}}>
       <TextInput placeholder="QR Value" />
-      <QRCode value="www.codemobiles.com" />
+      <QRCode value={qrValue != '' ? qrValue : 'www.codemobiles.com'} />
     </View>
   );
 };
