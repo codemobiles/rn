ac605e1b94c772b43fad38256506d4f214126207
codemobiles <chaiyasit.t@gmail.com>
2020-03-04 15:20:06 +0700

    1234

--- cmQRFn/App.js
+++ cmQRFn/App.js
@@ -1,11 +1,9 @@
-import React from 'react'
-import { View, Text } from 'react-native'
-import AppNavigator from './src/AppNavigator'
+import React from 'react';
+import {View, Text} from 'react-native';
+import AppNavigator from './src/AppNavigator';
 
 const App = () => {
-  return (
-    <AppNavigator/>
-  )
-}
+  return <AppNavigator />;
+};
 
-export default App
+export default App;
--- cmQRFn/src/AppNavigator.js
+++ cmQRFn/src/AppNavigator.js
@@ -1,66 +1,65 @@
-import React, { Component } from "react";
-import {
-  Image
-} from "react-native";
+import React, {Component} from 'react';
+import {Image} from 'react-native';
 
-import TabQRcode from "./TabQRcode";
-import TabScanner from "./TabScanner";
-import ScannerScreen from './ScannerScreen'
+import TabQRcode from './TabQRcode';
+import TabScanner from './TabScanner';
+import ScannerScreen from './ScannerScreen';
 
-import { createStackNavigator } from 'react-navigation-stack';
-import { createBottomTabNavigator } from 'react-navigation-tabs';
-import { createAppContainer} from "react-navigation";
-
-
+import {createStackNavigator} from 'react-navigation-stack';
+import {createBottomTabNavigator} from 'react-navigation-tabs';
+import {createAppContainer} from 'react-navigation';
 
 const tab1_Option = {
-  tabBarLabel: "QRcode",
-  tabBarIcon: ({ focused }) => (
+  tabBarLabel: 'QRcode',
+  tabBarIcon: ({focused}) => (
     <Image
       style={{
         height: 28,
-        width: 28
+        width: 28,
       }}
       resizeMode="contain"
       source={
         focused
-          ? require("./assets/img/ic_qr_code_press.png")
-          : require("./assets/img/ic_qr_code_normal.png")
+          ? require('./assets/img/ic_qr_code_press.png')
+          : require('./assets/img/ic_qr_code_normal.png')
       }
     />
-  )
-}
+  ),
+};
 
 const tab2_Option = {
-  tabBarLabel: "Scanner",
-  tabBarIcon: ({ focused }) => (
+  tabBarLabel: 'Scanner',
+  tabBarIcon: ({focused}) => (
     <Image
       style={{
         height: 28,
-        width: 28
+        width: 28,
       }}
       resizeMode="contain"
       source={
         focused
-        ? require("./assets/img/ic_qr_scan_press.png")
-        : require("./assets/img/ic_qr_scan_normal.png")     
+          ? require('./assets/img/ic_qr_scan_press.png')
+          : require('./assets/img/ic_qr_scan_normal.png')
       }
     />
-  )
-}
+  ),
+};
 
-const TabScreen = createBottomTabNavigator({
-  writeScreen: {screen: TabQRcode, navigationOptions: ()=> tab1_Option},
-  readScreen: {screen: TabScanner, navigationOptions: ()=> tab2_Option},
-}, {
-  initialRouteName:"writeScreen"
-})
 
-const AppStack = createStackNavigator({
-  tabScreen: {screen: TabScreen, navigationOptions: {headerShown: false}},
-  scanScreen : {screen: ScannerScreen}
-}, {
-  initialRouteName: "tabScreen"
-})
+const TabsScreen = createBottomTabNavigator({
+  generateScreen: {screen: TabQRcode, navigationOptions: () => tab1_Option},
+  scanerScreen: {screen: TabScanner, navigationOptions: () => tab2_Option},
+}, {initialRouteName: "generateScreen"});
 
-export default createAppContainer(AppStack)
+const AppStack = createStackNavigator(
+  {
+    tabScreen: {screen: TabsScreen, navigationOptions: {headerShown: false}},
+    scanScreen: {screen: ScannerScreen},
+  },
+  {
+    initialRouteName: 'tabScreen',
+  },
+);
+
+// HOC
+export default createAppContainer(AppStack);
