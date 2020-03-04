f389425f7b826c08a3af44dd2fc3e7c6d384a316
codemobiles <chaiyasit.t@gmail.com>
2020-03-04 10:15:13 +0700

    1234

--- cmAuthenFn/src/CameraScreen.js
+++ cmAuthenFn/src/CameraScreen.js
@@ -25,8 +25,68 @@ const CameraScreen = () => {
         resizeMode="contain"
         source={require('./assets/img/banner_react_qr_camera.png')}
       />
+
+      {/* Buttons section */}
+      <View style={{flexDirection: 'row', justifyContent: 'space-between',}}>
+
+      <Text style={styles.text}>CAMERA</Text>
+
+      </View>
     </ImageBackground>
   );
 };
 
 export default CameraScreen;
+
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+  },
+
+  button: {
+    marginBottom: 10,
+    height: 40,
+    padding: 8,
+    alignSelf: 'center',
+    justifyContent: 'center',
+    alignItems: 'center',
+    fontSize: 20,
+    borderColor: '#FFFFFF',
+    borderWidth: 1,
+    borderRadius: 5,
+    backgroundColor: 'transparent',
+  },
+  upload_button: {
+    borderRadius: 15,
+    marginBottom: 10,
+    fontWeight: 'bold',
+    width: 300,
+    height: 50,
+    marginBottom: 20,
+    alignSelf: 'center',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fa4a4d',
+  },
+  text: {
+    color: '#FFFFFF',
+    fontWeight: 'normal',
+  },
+  text_upload: {
+    color: '#FFFFFF',
+    fontWeight: 'bold',
+  },
+  description: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignSelf: 'center',
+    fontSize: 10,
+    color: '#FFFFFF',
+  },
+});
+
