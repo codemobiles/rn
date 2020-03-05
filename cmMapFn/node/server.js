c21db1abc9e9b6c7e9d73a543d1ce15d164c9ec9
codemobiles <chaiyasit.t@gmail.com>
2020-03-05 13:58:48 +0700

    1234

--- cmMapFn/node/server.js
+++ cmMapFn/node/server.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const position = require('./model_position');
 
 // http://localhost:3000/record_position
 // {"lat": "123", "lng": "5555"}
 app.use(bodyParser.json());
 
-app.post('/record_position', (req, res) => {
+app.post('/record_position', async (req, res) => {
   console.log(JSON.stringify(req.body));
+  await position.create(req.body);
   res.json({result: 'ok', message: req.body});
+});
+
+app.get('/position', async (req, res) => {
+  let result = await position.findAll();
+  res.json(result);
 });
 
 app.listen(3000, () => {
