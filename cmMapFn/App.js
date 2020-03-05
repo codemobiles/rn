65664fc744257f7b9fdb30fd6d6b0e51f0397be2
codemobiles <chaiyasit.t@gmail.com>
2020-03-05 11:24:56 +0700

    1234

--- cmMapFn/App.js
+++ cmMapFn/App.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
-import {Text, View} from 'react-native';
+import {View, Text} from 'react-native';
 import AppNavigator from './src/AppNavigator';
-
 export default class App extends Component {
   render() {
     return <AppNavigator />;
