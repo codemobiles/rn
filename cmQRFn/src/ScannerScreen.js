e37450264d9c470eb4c0cb5c0bd29e41985085f1
codemobiles <chaiyasit.t@gmail.com>
2020-03-05 09:20:37 +0700

    1234

--- cmQRFn/src/ScannerScreen.js
+++ cmQRFn/src/ScannerScreen.js
@@ -1,12 +1,70 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useState, useEffect, useRef} from 'react';
 
-const ScannerScreen = () => {
-  return (
-    <View>
-      <Text></Text>
-    </View>
-  );
+import {View, StyleSheet, Text, TouchableOpacity} from 'react-native';
+
+import QRCodeScanner from 'react-native-qrcode-scanner';
+
+const ScannerScreen = props => {
+  const scannerRef = useRef(null);
+
+  const showScanner = () => {
+    return (
+      <View style={{flex: 1, backgroundColor: 'black'}}>
+        <QRCodeScanner
+          ref={scannerRef}
+          showMarker
+          style={{flex: 1}}
+          bottomContent={
+            <TouchableOpacity
+              onPress={() => scannerRef.current.reactivate()}
+              style={styles.buttonTouchable}>
+              <Text style={styles.buttonText}>SCAN</Text>
+            </TouchableOpacity>
+          }
+        />
+      </View>
+    );
+  };
+
+  return <View style={styles.container}>{showScanner(props, scannerRef)}</View>;
 };
 
 export default ScannerScreen;
+
+ScannerScreen.navigationOptions = ({navigation}) => {
+  return {
+    title: 'Scanner',
+    headerStyle: {
+      backgroundColor: '#3F51B5',
+    },
+    headerTintColor: '#FFFFFF',
+    headerTitleStyle: {color: '#fff'},
+  };
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+  },
+  centerText: {
+    flex: 1,
+    fontSize: 18,
+    padding: 32,
+    color: '#777',
+  },
+  buttonText: {
+    fontSize: 21,
+    fontWeight: 'bold',
+    color: '#FFF',
+  },
+  buttonTouchable: {
+    height: 50,
+    width: '100%',
+    alignSelf: 'center',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fa4a4d',
+  },
+});
