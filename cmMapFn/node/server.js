a0e39d2148181db8d51d1c23fb8b931001db89c6
codemobiles <chaiyasit.t@gmail.com>
2020-03-05 13:41:32 +0700

    1234

---
+++ cmMapFn/node/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+// http://localhost:3000/record_position
+app.use(bodyParser.json());
+
+app.post('/record_position', (req, res) => {
+  console.log(JSON.stringify(req.body));
+  res.json({result: 'ok', message: req.body});
+});
+
+app.listen(3000, () => {
+  console.log('Ready..');
+});
