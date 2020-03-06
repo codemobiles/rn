8229ad00bab9a81b567199d2abecc7b87a4e7f37
codemobiles <chaiyasit.t@gmail.com>
2020-03-06 11:52:01 +0700

    1234

--- lekRedux/src/actions/login.action.js
+++ lekRedux/src/actions/login.action.js
@@ -15,21 +15,18 @@ export const setStateToSuccess = payload => ({
   payload,
 });
 
-export const login = ({username, password, navigation})=>{
+export const login = ({username, password, navigation}) => {
+  return async dispatch => {
+    dispatch(setStateToFetching());
 
-   return dispatch=>{
-       dispatch(setStateToFetching())
-      
-
-       let _regUsername = await AsyncStorage.getItem('username');
-       let _regPassword = await AsyncStorage.getItem('password');
-       if (_regUsername == username && _regPassword == password) {
-         await AsyncStorage.setItem('already_logged_in', 'yes');
-         dispatch(setStateToSuccess({result: 'ok', username}));
-         navigation.navigate('AppScene');
-       } else {
-         dispatch(setStateToFailed('Error, invalid account'));
-       }
-   }
-  
-}
+    let _regUsername = await AsyncStorage.getItem('username');
+    let _regPassword = await AsyncStorage.getItem('password');
+    if (_regUsername == username && _regPassword == password) {
+      await AsyncStorage.setItem('already_logged_in', 'yes');
+      dispatch(setStateToSuccess({result: 'ok', username}));
+      navigation.navigate('AppScene');
+    } else {
+      dispatch(setStateToFailed('Error, invalid account'));
+    }
+  };
+};
