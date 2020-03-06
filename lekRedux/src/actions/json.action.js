eedb85c8436503000f9b7c7afc6681390c0f9280
codemobiles <chaiyasit.t@gmail.com>
2020-03-06 13:44:16 +0700

    1234

--- lekRedux/src/actions/json.action.js
+++ lekRedux/src/actions/json.action.js
@@ -1,4 +1,5 @@
 import {FEED_FETCHING, FEED_FAILED, FEED_SUCCESS} from '../constants';
+import axios from 'axios';
 
 export const setStateToFetching = () => ({
   type: FEED_FETCHING,
@@ -17,8 +18,21 @@ export const setStateToSuccess = payload => ({
 export const feed = () => {
   return dispatch => {
     dispatch(setStateToFetching());
-    setTimeout(() => {
-      dispatch(setStateToSuccess([1, 2, 3, 4]));
-    }, 1000);
+    doFeed(dispatch)
   };
+};
+
+const doFeed = async (dispatch) => {
+  console.log('JSON Created');
+
+  let url = 'http://codemobiles.com/adhoc/youtubes/index_new.php';
+  let regUsername = 'admin'; // await AsyncStorage.getItem('username')
+  let regPassword = 'password'; // await AsyncStorage.getItem('password')
+  let data = `username=${regUsername}&password=${regPassword}&type=foods`;
+
+  let result = await axios.post(url, data);
+
+  // Destructuring
+  const {youtubes} = result.data;
+  dispatch(setStateToSuccess(youtubes));
 };
