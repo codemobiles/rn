3019149f70143792631189c6fc3a9661e27fa426
codemobiles <chaiyasit.t@gmail.com>
2020-03-06 11:05:23 +0700

    1234

--- lekRedux/index.js
+++ lekRedux/index.js
@@ -6,4 +6,12 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
+// Redux Begin
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
+import reducers from './src/reducers';
+// Redux End
+
 AppRegistry.registerComponent(appName, () => App);
